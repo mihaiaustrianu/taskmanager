a691a1f5a92c3e97c9d01b0412aebb9f4f1b6ba4
Mihai Austrianu <mihai.austrianu@netcentric.biz>
2025-07-06 11:10:22 +0300

    TM-upadte-task Added button for editing

--- js/store/TaskStore.js
+++ js/store/TaskStore.js
@@ -1,4 +1,4 @@
-import { STORAGE_KEY_TASKS } from "../constants.js";
+import { STORAGE_KEY_TASKS } from '../constants.js';
 export default class TaskStore {
     constructor() {
         this.tasks = this.loadFromStorage() || [];
@@ -20,24 +20,38 @@ export default class TaskStore {
     }
 
     updateTaskStatus(taskId, newStatus) {
-      const task = this.tasks.find(task => task.id === Number(taskId));
-      if (task) {
-          task.status = newStatus;
-          this.saveToStorage();
-          return true;
-      }
-      return false;
-  }
+        const task = this.tasks.find((task) => task.id === Number(taskId));
+        if (task) {
+            task.status = newStatus;
+            this.saveToStorage();
+            return true;
+        }
+        return false;
+    }
+
+    getTaskById(taskID) {
+        return this.tasks.find((task) => task.id === Number(taskID));
+    }
+
+    updateTask(updatedTask) {
+        const index = this.tasks.findIndex(
+            (task) => task.id === updatedTask.id
+        );
+        if (index !== -1) {
+            this.tasks[index] = updatedTask;
+            this.saveToStorage();
+        }
+    }
 
     loadFromStorage() {
         const data = localStorage.getItem(STORAGE_KEY_TASKS);
         if (data) {
-            return JSON.parse(data)
+            return JSON.parse(data);
         }
         return null;
     }
 
-    saveToStorage(){
+    saveToStorage() {
         localStorage.setItem(STORAGE_KEY_TASKS, JSON.stringify(this.tasks));
     }
 }
